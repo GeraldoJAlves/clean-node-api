08201170834a66918b43977fe3e3454f1661251e
GeraldoJAlves <junior.alves.2007@gmail.com>
2020-11-22 21:11:57 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
@@ -9,6 +9,9 @@ export const MongoHelper = {
     })
   },
   async disconnect (): Promise<void> {
-    await this.client.close()
+    if (this.client) { await this.client.close() }
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
