f0e3e1926981df64fccfe431d47e22f877e664c0
GeraldoJAlves <junior.alves.2007@gmail.com>
2020-11-22 21:12:18 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,34 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+
+import { AccountMongoRepository } from './account'
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
+describe('Account Mongo Repository', () => {
+  const uriMongo: string = process.env.MONGO_URL ? process.env.MONGO_URL : ''
+
+  beforeAll(async () => {
+    if (uriMongo) { await MongoHelper.connect(uriMongo) }
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = makeSut()
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
