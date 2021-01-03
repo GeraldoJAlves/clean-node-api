6f62f47fcc5290389061349ecb2120d04765faab
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-01-03 13:58:57 -0300

    test: ensure DbLoadAccountByToken returns null if LoadAccountByTokenRepository returns null

--- src/data/usescases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usescases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -4,7 +4,7 @@ import { AccountModel, LoadAccountByTokenRepository, Decrypter } from './db-load
 const makeLoadAccountByTokenRepository = (): LoadAccountByTokenRepository => {
   class LoadAccountByTokenRepositoryStub implements LoadAccountByTokenRepository {
     async loadByToken (accessToken: string, role?: string): Promise<AccountModel> {
-      return null
+      return makeFakeAccount()
     }
   }
   return new LoadAccountByTokenRepositoryStub()
@@ -37,12 +37,26 @@ const makeSut = (): SutTypes => {
   }
 }
 
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email',
+  password: 'hashed_password'
+})
+
 describe('DbLoadAccountByToken usecases', () => {
   test('Should call LoadAccountByTokenRepository with correct values', async () => {
     const { sut, loadAccountByTokenRepositoryStub } = makeSut()
     const loadByTokenSpy = jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken')
     await sut.load('any_token', 'any_role')
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role')
+  })
+
+  test('Should return null if LoadAccountByTokenRepository returns null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(Promise.resolve(null))
+    const account = await sut.load('any_token')
+    expect(account).toBeNull()
   })
 
   test('Should call Decrypter with correct values', async () => {
