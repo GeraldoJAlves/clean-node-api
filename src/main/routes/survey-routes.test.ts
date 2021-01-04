c40ccd6d41cfaf7ef433db5d0681844166022476
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-01-04 00:08:59 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,10 +2,13 @@ import { Collection } from 'mongodb'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 describe('Surveys Routes', () => {
-  const uriMongo: string = process.env.MONGO_URL ?? ''
+  const uriMongo: string = process.env.MONGO_URL
   let surveyCollection: Collection
+  let accountCollection: Collection
 
   beforeAll(async () => {
     if (uriMongo) { await MongoHelper.connect(uriMongo) }
@@ -14,6 +17,8 @@ describe('Surveys Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   afterAll(async () => {
@@ -34,6 +39,36 @@ describe('Surveys Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const { ops: [{ _id: id }] } = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const accessToken = await sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/v1/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+          }, {
+            answer: 'other_answer'
+          }]
+        })
+        .expect(204)
     })
   })
 })
