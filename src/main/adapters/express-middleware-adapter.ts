d120204b0b07a1da99b9b497d885aed2a4f88071
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-01-03 23:53:40 -0300

    feat: ensure add survey route returns 403 if no access token is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+import { HttpRequest, Middleware } from '../../presentation/protocols'
+
+export const expressMiddlewareAdapter = (middleware: Middleware): RequestHandler => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const { statusCode, body } = await middleware.handle(httpRequest)
+    if (statusCode === 200) {
+      Object.assign(req, body)
+      next()
+    } else {
+      res.status(statusCode).json({
+        error: body.message
+      })
+    }
+  }
+}
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
+import { expressMiddlewareAdapter } from '../adapters/express-middleware-adapter'
 import { expressRouteAdapter } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
-  router.post('/surveys', expressRouteAdapter(makeAddSurveyController()))
+  const adminAuth = expressMiddlewareAdapter(makeAuthMiddleware('admin'))
+  router.post('/surveys', adminAuth, expressRouteAdapter(makeAddSurveyController()))
 }
