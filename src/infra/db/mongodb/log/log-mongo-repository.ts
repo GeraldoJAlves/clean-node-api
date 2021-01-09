05f9a873e280226a5c3b22512b8814fbfed32135
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-01-09 19:38:43 -0300

    refactor: change name variable

--- src/infra/db/mongodb/log/log-mongo-repository.ts
+++ src/infra/db/mongodb/log/log-mongo-repository.ts
@@ -2,7 +2,7 @@ import { LogErrorRepository } from '@/data/protocols/db/log'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 export class LogMongoRepository implements LogErrorRepository {
   async logError (stackError: string): Promise<void> {
-    const accountCollection = await MongoHelper.getCollection('errors')
-    await accountCollection.insertOne({ stackError, date: new Date() })
+    const errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.insertOne({ stackError, date: new Date() })
   }
 }
