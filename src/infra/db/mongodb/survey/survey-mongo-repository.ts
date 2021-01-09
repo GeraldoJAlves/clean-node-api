dc630e84d27b5565b9ae9921a408fdc806c2dd4e
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-01-09 19:22:29 -0300

    refactor: change name variable

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -7,19 +7,19 @@ import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   async add (data: AddSurveyModel): Promise<void> {
-    const accountCollection = await MongoHelper.getCollection('surveys')
-    await accountCollection.insertOne(data)
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.insertOne(data)
   }
 
   async loadAll (): Promise<SurveyModel[]> {
-    const accountCollection = await MongoHelper.getCollection('surveys')
-    const surveys = await accountCollection.find().toArray()
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
     return surveys
   }
 
   async loadById (id: string): Promise<SurveyModel> {
-    const accountCollection = await MongoHelper.getCollection('surveys')
-    const survey = await accountCollection.findOne({ _id: id })
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const survey = await surveyCollection.findOne({ _id: id })
     return survey
   }
 }
