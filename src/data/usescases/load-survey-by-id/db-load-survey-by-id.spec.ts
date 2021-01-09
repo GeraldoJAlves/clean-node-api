6c7a4188f1e3a50586d0557463f9c9d077db82fc
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-01-09 14:18:59 -0300

    feat: ensure DbLoadSurveyById throws if LoadSurveyByIdRepository throws

--- src/data/usescases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usescases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -52,4 +52,11 @@ describe('DbLoadSurveyById usecases', () => {
     await sut.loadById('any_id')
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('Should throw if LoadSurveyByIdRepository throw', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById').mockReturnValueOnce(Promise.reject(new Error()))
+    const promise = sut.loadById('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
