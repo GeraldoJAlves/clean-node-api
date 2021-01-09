b3bd061901a50c4eddd3bb043c3d2dea7211b91c
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-01-09 14:16:40 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct value

---
+++ src/data/usescases/load-survey-by-id/db-load-survey-by-id-protocols.ts
@@ -0,0 +1,3 @@
+export { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+export { SurveyModel } from '@/domain/models/survey'
+export { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
