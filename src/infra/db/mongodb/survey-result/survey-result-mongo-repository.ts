d808ee0e86e1822620d7372ee9761e1283c3ead8
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-01-09 19:29:52 -0300

    feat: ensure SurveyResultMongoRepository add a survey-result on success

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,12 @@
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyCollection = await MongoHelper.getCollection('survey-result')
+    const result = await surveyCollection.insertOne(data)
+    return MongoHelper.map(result.ops[0])
+  }
+}
