dfecb079b13b628ae36921bfaaef88ad704886ef
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-02-01 01:22:32 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct id

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -10,10 +10,12 @@ export class LoadSurveyResultController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      const surveyId = httpRequest.params.surveyId
+      const survey = await this.loadSurveyById.loadById(surveyId)
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      await this.loadSurveyResult.load(surveyId)
       return null
     } catch (err) {
       return serverError(err)
